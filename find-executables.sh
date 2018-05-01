69039c1cc7d2edd24e0080b5972cb37d410d9eae
Erik Sjölund <erik.sjolund@gmail.com>
2018-05-01 09:08:52 +0200

    Add bash script find-executables.sh that finds executables on the unix system
    The bash script traverses the file system and
    prints the filenames of the executables found.
    By specifying the command line option ("setuid") only
    executables with the setuid bit are printed.

---
+++ find-executables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+extra_arguments=""
+
+if [ "$#" -eq 1 ]; then
+  if [ $1 = "setuid" ]; then
+    extra_arguments="-perm /4000"
+  fi
+fi
+
+sudo find / \
+     -path /proc -prune \
+  -o -path /sys  -prune \
+  -o -path /run  -prune \
+  -o -path /var/lib/lxd/storage-pools -prune \
+  -o -path /home -prune \
+  -o -path /tmp -prune \
+  -o -path /var/tmp -prune \
+  -o -type f -perm -u=x $extra_arguments -print
